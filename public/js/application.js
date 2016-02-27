76ea4172882f70dfd23d22983151874b5626fb89
ZenStein <shaneenterprises@gmail.com>
2016-02-27 12:17:24 -0800

    angularize bones, add ignore

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,6 @@
-$(document).ready(function () {
+angular.module('main',[])
+.controller('mainController', ['$scope', '$http', function($scope, $http){
+	alert('controller hits');
+	$scope.test = {tester:'this is a test'}
 
-});
+}])
